96344284ec49da88519f2ccb3a42942a0cef1279
Vladislav <vladislav14141@yandex.ru>
2019-03-17 21:26:00 +1000

    Initial Commit

---
+++ 10 years ago test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  10 years ago test
+//
+//  Created by Миронов Влад on 17/03/2019.
+//  Copyright © 2019 Миронов Влад. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
